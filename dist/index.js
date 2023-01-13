66afd02748f831a25b521f30686f521064fa0e19
Sergey <50526865+Kosrotoff@users.noreply.github.com>
2023-01-13 18:48:30 +0700

    feat: rename export factories

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,7 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./interfaces"), exports);
+exports.RootRouterFactory = exports.RouterFactory = exports.CuratorFactory = void 0;
+var interfaces_1 = require("./interfaces");
+Object.defineProperty(exports, "CuratorFactory", { enumerable: true, get: function () { return interfaces_1.Curator__factory; } });
+Object.defineProperty(exports, "RouterFactory", { enumerable: true, get: function () { return interfaces_1.Router__factory; } });
+Object.defineProperty(exports, "RootRouterFactory", { enumerable: true, get: function () { return interfaces_1.RootRouter__factory; } });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export * from './interfaces';
+export {
+    Curator,
+    Router,
+    RootRouter,
+    Curator__factory as CuratorFactory,
+    Router__factory as RouterFactory,
+    RootRouter__factory as RootRouterFactory
+} from './interfaces';
